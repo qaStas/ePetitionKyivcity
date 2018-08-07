82df6f7b0fa8c962a2342303860539c959caf4dd
Stanislav <bsl604@gmail.com>
2018-08-07 18:16:37 +0300

    [qaStas] LoginTestWithOutPageObject was add

---
+++ src/test/java/loginTests/LoginTestWithOutPageObject.java
@@ -0,0 +1,39 @@
+package loginTests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class LoginTestWithOutPageObject {
+    // тест быстро создаётся, но не поддерживается
+
+    WebDriver webDriver;
+
+    @Before
+    public void setUp(){
+        File file = new File("./src/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        webDriver = new ChromeDriver();
+    }
+
+    @Test
+    public void validLogIn(){
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        webDriver.get("https://id.kyivcity.gov.ua/sign_in");
+
+        //webDriver.findElement( );
+
+    }
+
+    @After
+    public void tearDown(){
+        webDriver.quit();
+    }
+
+}
