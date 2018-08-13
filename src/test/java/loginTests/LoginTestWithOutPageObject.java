69676b5bb949df5f35b83af55f64f1be72bd9c04
Stanislav <bsl604@gmail.com>
2018-08-13 20:20:41 +0300

    [qaStas] test case click Next

--- src/test/java/loginTests/LoginTestWithOutPageObject.java
+++ src/test/java/loginTests/LoginTestWithOutPageObject.java
@@ -3,6 +3,7 @@ package loginTests;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -25,9 +26,13 @@ public class LoginTestWithOutPageObject {
     public void validLogIn() {
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        webDriver.get("https://id.kyivcity.gov.ua/sign_in");
+        webDriver.get("https://id.kitsoft.kiev.ua/sign_in");
 
-        //webDriver.findElement( );
+        webDriver.findElement(By.name("email")).clear();
+        webDriver.findElement(By.name("email")).sendKeys("bsl604@gmail.com");
+
+        webDriver.findElement(By.className("pull-right")).click();
+        webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
     }
 
