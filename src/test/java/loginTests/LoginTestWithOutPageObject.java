545ab6d972b33bcb575949d3af3360ce897cb844
Stanislav <bsl604@gmail.com>
2018-08-14 18:14:43 +0300

    [qaStas] bug when click Next

--- src/test/java/loginTests/LoginTestWithOutPageObject.java
+++ src/test/java/loginTests/LoginTestWithOutPageObject.java
@@ -31,8 +31,19 @@ public class LoginTestWithOutPageObject {
         webDriver.findElement(By.name("email")).clear();
         webDriver.findElement(By.name("email")).sendKeys("bsl604@gmail.com");
 
-        webDriver.findElement(By.className("pull-right")).click();
+        //webDriver.findElement(By.className("pull-right")).click();
+        //webDriver.findElement(By.xpath(".//*[@class='btn btn-submit btn-email-submit-js']")).click();
+
         webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        webDriver.findElement(By.className("form-control ng-pristine ng-invalid ng-invalid-required ng-touched")).clear();
+
+      //  webDriver.findElement(By.name("password")).clear();
+      //  webDriver.findElement(By.name("password")).sendKeys("bsl604fast");
+
+        webDriver.findElement(By.tagName("button")).click();
+
+
 
     }
 
